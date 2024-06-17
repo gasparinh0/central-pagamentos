bb1e5c88fdd5bb1683bcd7f1503f63cdb1b48357
Gaspari <lcsgaspari@gmail.com>
2024-06-17 11:34:46 -0300

    adding the react structure and constructing the navbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+//Import do CSS
 import './App.css';
+
+//Import do react-icons
+import { TbPigMoney } from "react-icons/tb";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="flex p-8 bg-slate-200">
+  <div className='bg-slate-400 p-2 mr-3 content-none rounded-full'>
+    <TbPigMoney size={60}/>
+  </div>
+  <div>
+    <h1 className='text-3xl'>Central do fiado</h1>
+    <p className='text-2xl font-light'>Bem vindo!</p>
+  </div>
+  <div className='flex ml-auto'>
+    <div className='bg-slate-400 mr-8 flex justify-center align-middle items-center p-4 rounded-full'>
+      <h1 className='text-3xl'>Cadastrar cliente</h1>
     </div>
+    <div className='bg-slate-400 mr-8 flex justify-center align-middle items-center p-4 rounded-full'>
+     <h1 className='text-3xl'>Cadastrar pedido</h1>
+    </div>
+  </div>
+</div>
   );
 }
 
